2bfe2e2b1c6f8ea823ad3ed13ed5594b97dc6d0f
mjbae <0131mj@gmail.com>
2019-03-23 07:08:04 +0900

    Add Get Customers TREST API Module

--- client/src/App.js
+++ client/src/App.js
@@ -18,34 +18,25 @@ const styles = theme => ({
   }
 })
 
-const customers = [
-  {
-    'id': 1,
-    'image':'https://placeimg.com/64/64/any',
-    'name':'김길동',
-    'birthday': '981222',
-    'gender' :'남자',
-    'job': '개발자' 
-  },
-  {
-    'id': 2,
-    'image':'https://placeimg.com/64/64/1',
-    'name':'박길동',
-    'birthday': '991222',
-    'gender' :'남자',
-    'job': '대학생' 
-  },
-  {
-    'id': 3,
-    'image':'https://placeimg.com/64/64/2',
-    'name':'최길동',
-    'birthday': '961222',
-    'gender' :'여자',
-    'job': '대학원생' 
-  },
-]
 
 class App extends Component {
+
+  state = {
+    customers: ""
+  }
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({customers: res}))
+      .catch(err => console.log(err))
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
   render() {
     const {classes} = this.props;
     return (
@@ -62,17 +53,20 @@ class App extends Component {
             </TableRow>
           </TableHead>
           <TableBody>
-          {customers.map(c => (
-          <Customer
-            key={c.id}
-            id={c.id}
-            image={c.image}
-            name={c.name}
-            birthday={c.birthday}
-            gender={c.gender}
-            job={c.job}
-        />
-        ))}
+          {this.state.customers 
+            ? this.state.customers.map(c => (
+                <Customer
+                  key={c.id}
+                  id={c.id}
+                  image={c.image}
+                  name={c.name}
+                  birthday={c.birthday}
+                  gender={c.gender}
+                  job={c.job}
+              />
+              ))
+            : ""
+        }
           </TableBody>
         </Table> 
       </Paper>
