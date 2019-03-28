c5c9c7fa0842156ea49cc2c5d8c55bc306aa9e8a
mjbae <0131mj@gmail.com>
2019-03-29 06:20:03 +0900

    Implement Customer Add Module & Customer Listt Refresh Module

--- client/src/App.js
+++ client/src/App.js
@@ -25,10 +25,25 @@ const styles = theme => ({
 
 class App extends Component {
 
-  state = {
-    customers: "",
-    completed : 0
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+      customers: "",
+      completed : 0
+    }
   }
+
+  stateRefresh = () => {
+    this.setState({
+        customers: "",
+        completed : 0
+    })
+    this.callApi()
+      .then(res => this.setState({customers: res}))
+      .catch(err => console.log(err))
+  }
+  
 
   componentDidMount() {
     this.timer = setInterval(this.progress, 20);
@@ -92,7 +107,7 @@ class App extends Component {
             </TableBody>
           </Table> 
         </Paper>
-        <CustomerAdd/>
+        <CustomerAdd stateRefresh={this.stateRefresh}/>
       </div>
     );
   }
