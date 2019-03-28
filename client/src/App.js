215dbef2edfed882c11179d773d102097e9a7d23
mjbae <0131mj@gmail.com>
2019-03-29 06:49:05 +0900

    Add Customer Delete Module

--- client/src/App.js
+++ client/src/App.js
@@ -79,6 +79,7 @@ class App extends Component {
                 <TableCell>생년월일</TableCell> 
                 <TableCell>성별</TableCell> 
                 <TableCell>직업</TableCell> 
+                <TableCell>설정</TableCell> 
               </TableRow>
             </TableHead>
             <TableBody>
@@ -92,6 +93,7 @@ class App extends Component {
                     birthday={c.birthday}
                     gender={c.gender}
                     job={c.job}
+                    stateRefresh={this.stateRefresh}
                 />
                 ))
               : <TableRow>
